341a3baf73728d6cdab3b0d953907e432a2a2956
leo barrientos <leobarrientos@gmail.com>
2019-12-13 16:25:30 -0300

    integracion BBDD. getCustomer

---
+++ src/main/java/com/imagemaker/demo/entity/Customer.java
@@ -0,0 +1,51 @@
+package com.imagemaker.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Long id;
+  private String firstName;
+  private String lastName;
+  private String address;
+
+  public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+protected Customer() {}
+
+  public Customer(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "Customer[id=%d, firstName='%s', lastName='%s', address='%s']",
+        id, firstName, lastName, address);
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+}
